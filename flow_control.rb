198623ca738cb617a964a81627325f5a5ed08453
David Dinh <dmdinh22@gmail.com>
2014-12-05 02:49:05 -0800

    final version

--- flow_control.rb
+++ flow_control.rb
@@ -11,46 +11,46 @@ puts 2 < 10
 puts '2' < '10'
 puts "----"
 # Branching
-# puts "Hello, what's your name?"
-# name = gets.chomp
-# puts "Hello, " + name + "."
+puts "Hello, what's your name?"
+name = gets.chomp
+puts "Hello, " + name + "."
 
 
-# if name == "David" || name == "Katy"
-#   puts "What a dope name!"
-# end
-
-puts "----"
-
-# puts "I am a fortune-teller. Tell me your name:"
-# name = gets.chomp
-
-# if name == "David"
-#   puts "I see amazing things in your near future."
-# else
-#   puts "Your future is.... oh gosh! Look at the time!"
-#   puts "I really have to go, sorry friend!"
-# end
+if name == "David" || name == "Katy"
+  puts "What a dope name!"
+end
 
 puts "----"
 
-# puts "Hello, and welcome to seventh grade English."
-# puts "My name is Mrs. Gabbard. And your name is...?"
-# name = gets.chomp
+puts "I am a fortune-teller. Tell me your name:"
+name = gets.chomp
 
-# if name == name.capitalize
-#   puts "Please take a seat, " + name + "."
-# else
-#   puts name + "? You mean " + name.capitalize + ", right?"
-#   puts "Don't you even know how to spell your name?"
-#   reply = gets.chomp
+if name == "David"
+  puts "I see amazing things in your near future."
+else
+  puts "Your future is.... oh gosh! Look at the time!"
+  puts "I really have to go, sorry friend!"
+end
 
-#   if reply.downcase == "yes"
-#     puts "Sit your butt down then!"
-#   else
-#     puts "GET OUT NOW!!"
-#   end
-# end
+puts "----"
+
+puts "Hello, and welcome to seventh grade English."
+puts "My name is Mrs. Gabbard. And your name is...?"
+name = gets.chomp
+
+if name == name.capitalize
+  puts "Please take a seat, " + name + "."
+else
+  puts name + "? You mean " + name.capitalize + ", right?"
+  puts "Don't you even know how to spell your name?"
+  reply = gets.chomp
+
+  if reply.downcase == "yes"
+    puts "Sit your butt down then!"
+  else
+    puts "GET OUT NOW!!"
+  end
+end
 
 puts "----"
 
@@ -65,7 +65,7 @@ puts "----"
 
 # puts "Come again soon!"
 
-# puts "----"
+puts "----"
 
 i_am_chris = true
 i_am_purple = false
@@ -87,68 +87,68 @@ puts !i_am_chris # false
 
 puts "----"
 
-# request = ""
+request = ""
 
-# while request != "stop"
-#   puts "What would like to ask C to do?"
-#   request = gets.chomp
+while request != "stop"
+  puts "What would like to ask C to do?"
+  request = gets.chomp
 
-#   puts "You say, 'C please " + request + "'"
-#   puts "C's response:"
-#   puts "'C " + request + ".'"
-#   puts "'Papa " + request + ", too.'"
-#   puts "'Mama " + request + ", too.'"
-#   puts "'Ruby " + request + ", too.'"
-#   puts "'Nono " + request + ", too.'"
-#   puts "'Emma " + request + ", too.'"
-#   puts
-# end
+  puts "You say, 'C please " + request + "'"
+  puts "C's response:"
+  puts "'C " + request + ".'"
+  puts "'Papa " + request + ", too.'"
+  puts "'Mama " + request + ", too.'"
+  puts "'Ruby " + request + ", too.'"
+  puts "'Nono " + request + ", too.'"
+  puts "'Emma " + request + ", too.'"
+  puts
+end
 
 puts "----"
 #99 bottles
 
-# number_of_bottles = 99
-# while number_of_bottles != 0
-#   puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer. Take one down, pass it around, #{number_of_bottles - 1} bottles of beer on the wall."
-#   number_of_bottles = number_of_bottles - 1
-# end
+number_of_bottles = 99
+while number_of_bottles != 0
+  puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer. Take one down, pass it around, #{number_of_bottles - 1} bottles of beer on the wall."
+  number_of_bottles = number_of_bottles - 1
+end
 
 #deaf grandma with extension
 
-# puts "What do you want to say to your Grandma?"
-# answer1 = "blah blah blah"
-# answer2 = ""
-# answer3 = ""
-# #initialize 
+puts "What do you want to say to your Grandma?"
+answer1 = "blah blah blah"
+answer2 = ""
+answer3 = ""
+#initialize 
 
-# while answer1 != "BYE"
-#   answer1 = gets.chomp
-#   if answer1 != answer1.upcase
-#     puts "HUH?! SPEAK UP, SONNY!"
-#   else
-#     year = rand(1930..1950)
-#     puts "NO, NOT SINCE #{year}!"
-#   end
-# end
+while answer1 != "BYE"
+  answer1 = gets.chomp
+  if answer1 != answer1.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    year = rand(1930..1950)
+    puts "NO, NOT SINCE #{year}!"
+  end
+end
 
-# while answer2 != "BYE"
-#   answer2 = gets.chomp
-#   if answer2 != answer2.upcase
-#     puts "HUH?! SPEAK UP, SONNY!"
-#   else
-#     year = rand(1930..1950)
-#     puts "NO, NOT SINCE #{year}!"
-#   end
-# end
+while answer2 != "BYE"
+  answer2 = gets.chomp
+  if answer2 != answer2.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    year = rand(1930..1950)
+    puts "NO, NOT SINCE #{year}!"
+  end
+end
 
-# while answer3 != "BYE"
-#   answer3 = gets.chomp
-#   if answer3 != answer3.upcase
-#     puts "HUH?! SPEAK UP, SONNY!"
-#   else
-#     puts "BYE KID. :("
-#   end
-# end
+while answer3 != "BYE"
+  answer3 = gets.chomp
+  if answer3 != answer3.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "BYE KID. :("
+  end
+end
 
 # Leap Year
 
